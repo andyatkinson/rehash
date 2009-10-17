fb48e99b82f6910ea4caf073f9efd00c97233f28
Andy Atkinson <andy@webandy.com>
2009-10-17 00:04:43 -0500

    introducing new blue theme updating styling for site create screen
    more blue theme commits
    
    more theme tweaks to blue theme
    
    footer styles
    
    adding reset stylesheet and adjusting styles to accomodate
    
    more theme tweaks and adding some service icons
    
    adjusting articles index
    
    small tweaks to fix broken tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def tag_links_for_article(article)
+    links = []
+    article.tags.each do |tag|
+      links << link_to(tag.name, tagged_articles_path(:tag => tag.name))
+    end
+    links
+  end
+  
 end
