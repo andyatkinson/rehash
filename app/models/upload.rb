7d4bf1bcde5e6d2e2c041aa9551a6cd916b44444
Andy Atkinson <andy@webandy.com>
2010-07-25 16:03:01 -0400

    converting paperclip storage to s3

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,8 +1,12 @@
 class Upload < ActiveRecord::Base
   belongs_to :attachable, :polymorphic => true
   has_attached_file :data, 
+                    :storage => :s3,
+                    :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :path => ":attachment/:id/:style/:basename.:extension",
                     :styles => { :large => "800x800>",
                                  :small => '200x200#',
                                  :square => "100x100#" }
+                                     
   validates_attachment_size :data, :less_than => 5.megabytes
 end
