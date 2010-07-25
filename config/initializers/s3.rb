57b243576dfbf5759e57cc3a05cc0aa0e3db7ca2
Andy Atkinson <andy@webandy.com>
2010-07-25 16:51:38 -0400

    reword the initializer to separate development and production environment

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,15 @@
-s3config = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV]
+if Rails.env.development?
+  s3config = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")[RAILS_ENV]
+  AWS::S3::Base.establish_connection!(
+    :access_key_id     => s3config['access_key_id'],
+    :secret_access_key => s3config['secret_access_key']
+  )
+elsif Rails.env.production?
+  # intent is that production environment is on Heroku which uses config vars
+  # see: http://docs.heroku.com/config-vars
+  AWS::S3::Base.establish_connection!(
+    :access_key_id     => ENV['S3_KEY'],
+    :secret_access_key => ENV['S3_SECRET']
+  )
+end
 
-AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['S3_KEY'] || s3config['access_key_id'],
-  :secret_access_key => ENV['S3_SECRET'] || s3config['secret_access_key']
-)
