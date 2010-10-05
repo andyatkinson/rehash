b9a91709ed2b40a1f436aa808ff8ccc22b02ca57
Andy Atkinson <andy@webandy.com>
2010-10-05 00:56:16 -0400

    starting to address the deprecation warnings from the controller tests

--- config/environment.rb
+++ config/environment.rb
@@ -23,12 +23,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'haml', :version => '>= 2.2.2'
-  config.gem 'acts_as_markup'
-  config.gem 'friendly_id'
-  config.gem 'thoughtbot-paperclip', :lib => 'paperclip'
-  config.gem 'will_paginate', :version => '>= 2.3.15'
-  config.gem 'gravtastic'
+  config.gem 'haml',            :version => '3.0.21'
+  config.gem 'acts_as_markup',  :version => '1.3.3'
+  config.gem 'friendly_id',     :version => '2.1.3'
+  config.gem 'paperclip',       :version => '2.3.3'
+  config.gem 'will_paginate',   :version => '2.3.15'
+  config.gem 'gravtastic',      :version => '2.2.0'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
