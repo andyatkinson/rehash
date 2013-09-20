b471fdeaf14854592e061580c15b22e9003b37fb
Andy Atkinson <andyatkinson@gmail.com>
2013-09-19 22:36:53 -0400

    Send plain text notification emails for new comments and new contact form messages

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -1,14 +1,16 @@
 class SiteMailer < ActionMailer::Base
   
   def contact_form(contact_form)
-    @message = contact_form.message
     mail(to: WEBSITE_CONFIG['owner_email'], from: contact_form.email, 
-      subject: "Contact form email from #{contact_form.name}")
+      subject: "Contact form email from #{contact_form.name}",
+      body: contact_form.message,
+      content_type: "text/plain")
   end
   
   def comment_email(comment)
-    @message = comment.body
     mail(to: WEBSITE_CONFIG['owner_email'], from: comment.email,
-      subject: "New comment on #{comment.article.title}")
+      subject: "New comment on article: #{comment.article.title}",
+      body: comment.body,
+      content_type: "text/plain")
   end
 end
--- test/mailers/site_mailer_test.rb
+++ test/mailers/site_mailer_test.rb
@@ -11,6 +11,17 @@ class SiteMailerTest < ActionMailer::TestCase
     assert_equal [@contact_form.email], email.from
     assert_equal [WEBSITE_CONFIG['owner_email']], email.to
     assert_equal "Contact form email from #{@contact_form.name}", email.subject
-    
+    assert_equal "Getting in touch with you", @contact_form.message 
+  end
+
+  test "new comment notification to site owner email" do
+    @comment = Factory.build(:comment)
+    email = SiteMailer.comment_email(@comment).deliver
+
+    assert !ActionMailer::Base.deliveries.empty?
+    assert_equal [@comment.email], email.from
+    assert_equal [WEBSITE_CONFIG['owner_email']], email.to
+    assert_equal "New comment on article: #{@comment.article.title}", email.subject
+    assert_equal "That was an interesting article!", @comment.body
   end
 end
